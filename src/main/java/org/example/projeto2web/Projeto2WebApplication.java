e3edec7d25697fd3810fd61f911cbc9db755465d
pedrocarvalho131002 <163552278+pedrocarvalho131002@users.noreply.github.com>
2025-05-06 15:23:08 +0100

    Primeiro Commit

---
+++ src/main/java/org/example/projeto2web/Projeto2WebApplication.java
@@ -0,0 +1,13 @@
+package org.example.projeto2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projeto2WebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projeto2web/Projeto2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projeto2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto2WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
